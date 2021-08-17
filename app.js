c1cd9221007b14837d27ce4bdcf3e96a6bd6514e
Ignacio <ignaciolmartin@gmail.com>
2021-08-16 23:18:49 -0300

    Upload MVC with Express

---
+++ app.js
@@ -0,0 +1,28 @@
+// Call express module
+const express = require('express');
+
+// Call path module
+const path = require('path');
+
+// Build a express app
+const app = express();
+
+// Create PORT variable with process.env
+const PORT = process.env.PORT || 3001;
+
+// Set public folder
+const staticFolder = path.resolve(__dirname, './public');
+
+// Set app to use public folder
+app.use(express.static(staticFolder));
+
+// Set Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
+// Config port listen
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+})
+
